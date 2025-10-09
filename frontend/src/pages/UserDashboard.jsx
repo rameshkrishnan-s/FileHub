fadfaa94fc8ea750abc4fa71cd6c2bb556bcb8d1
Tharsan S <tharsantharsan05819@gmail.com>
2025-10-09 10:22:01 +0530

    Header Sticky

--- frontend/src/pages/UserDashboard.jsx
+++ frontend/src/pages/UserDashboard.jsx
@@ -149,7 +149,9 @@ const handleLogout = () => {
 
   return (
     <div className="min-h-screen bg-gray-50 text-gray-800 flex flex-col">
-      <Header />
+     <div className="sticky top-0 z-50 bg-white shadow">
+  <Header />
+</div>
       {/* Top Nav Bar */}
       <div className="fixed top-[108px] left-0 p-4 bg-white border-b border-gray-200 shadow z-50 w-full">
         <div className="flex items-center justify-between">
